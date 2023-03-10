66145a43f8f3f7d748976cf89f056034c868ca28
Affan218 <103178361+Gadget218@users.noreply.github.com>
2023-03-10 17:05:43 +0400

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,12 @@
 import openai
-
+import datetime
+import mysql.connector
+dbc=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='psswd',
+    database='Chatbot')
+mycursor = dbc.cursor()
 
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
@@ -26,12 +33,19 @@ def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, toke
 
 if __name__ == '__main__':
     conversation = list()
+    #D_t=datetime.datetime.now()
+    #mycursor.execute("CREATE TABLE Chat_h (id INT, Speaker VARCHAR(5), TEXT VARCHAR(255)")
     while True:
         user_input = input('USER: ')
+        mycursor.execute("INSERT INTO chat_h  (Speaker, TEXT) VALUES (%s,%s)", ('USER',user_input))
         conversation.append('USER: %s' % user_input)#takes input and adds it to memory
+        
         text_block = '\n'.join(conversation)#response of the chatbot
         prompt = open_file('prompt_chat.txt').replace('<<BLOCK>>', text_block)
         prompt = prompt + '\nBOT:'
         response = gpt3_completion(prompt)
         print('BOT:', response)
         conversation.append('BOT: %s' % response)#adds chatbot's answers to the memory
+        mycursor.execute("INSERT INTO chat_h  (Speaker, TEXT) VALUES (%s,%s)", ('BOT',response))
+        dbc.commit()
+        
